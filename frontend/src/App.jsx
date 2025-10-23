fe2a2348a95f0ac74736c9ebf8247e17f30a3d72
ahmedtahatekin <tekinahmedtaha456@gmail.com>
2025-10-23 23:54:37 +0300

    proje repository ve frontend dizini kuruldu.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <Navbar />
+  )
+}
+
+function Navbar() {
+  return (
+    <nav>
+      <h1 className="">My App</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default App
